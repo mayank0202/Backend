23406792a4797e815d06565f06f2e248de8519c8
Mayank Sharma <83959396+mayank0202@users.noreply.github.com>
2021-08-09 01:30:56 +0530

    Update database.js

--- database.js
+++ database.js
@@ -7,22 +7,4 @@ var pg = new Pg({
     port: 5432
 });
 module.exports = pg;
-// credentials of database
-// const env = require('./env');
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize(env.database, env.user, env.password, {
-//   host: env.host,
-//   dialect: env.dialect,
-//   operatorsAliases: false,
-//   pool: {
-//     max: env.max,
-//     min: env.pool.min,
-//     acquire: env.pool.acquire,
-//     idle: env.pool.idle
-//   }
-// });
-// const db = {};
-// db.Sequelize = Sequelize;
-// db.sequelize = sequelize;
-// db.users = require('./model')(sequelize, Sequelize);
-// module.exports=db;
+
